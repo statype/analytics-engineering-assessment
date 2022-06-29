0faf742b53a60a795c3485f1596b202937f5ae9b
David E Worth <dave@statype.com>
2022-06-29 21:15:48 +0000

    add devcontainer
    This devcontainer is based on Python+PostgreSQL and includes the
    rainbow-csv plugin for looking at raw data

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eou pipefail
+
+echo "Installing Postgres 14 Client in devcontainer"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql-pgdg.list > /dev/null
+
+sudo apt-get update
+sudo apt-get -y install bash-completion postgresql-client
+
+pip3 install -r /workspace/requirements.txt
+ln -s /workspace/.sqlfluff* ~/
